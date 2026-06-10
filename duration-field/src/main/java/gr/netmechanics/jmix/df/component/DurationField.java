3efa934cf08165717608133461fb2294c2c1d43e
panos <panos@netmechanics.gr>
2026-06-10 15:03:24 +0300

    feat: wire shortLabels into DurationField via setDatatype

--- duration-field/src/main/java/gr/netmechanics/jmix/df/component/DurationField.java
+++ duration-field/src/main/java/gr/netmechanics/jmix/df/component/DurationField.java
@@ -2,10 +2,22 @@ package gr.netmechanics.jmix.df.component;
 
 import java.time.Duration;
 
+import gr.netmechanics.jmix.df.datatype.DurationDatatype;
 import io.jmix.flowui.component.textfield.TypedTextField;
 
 /**
  * @author Panos Bariamis (pbaris)
  */
 public class DurationField extends TypedTextField<Duration> {
+
+    private boolean shortLabels = true;
+
+    public boolean isShortLabels() {
+        return shortLabels;
+    }
+
+    public void setShortLabels(final boolean shortLabels) {
+        this.shortLabels = shortLabels;
+        setDatatype(new DurationDatatype(shortLabels));
+    }
 }
